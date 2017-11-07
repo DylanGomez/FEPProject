4c9e192cb32d255054ba6ee8a24e933005b26174
alinoil <alinoil@live.nl>
2017-11-07 11:37:26 +0100

    List bug fix

--- src/app/hardware-defect/hardware-defect.component.ts
+++ src/app/hardware-defect/hardware-defect.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormDataService } from '../form-data/form-data.service';
+import { RouterModule, Routes, Router } from '@angular/router';
 declare var $: any;
 
 @Component({
@@ -18,7 +19,8 @@ export class HardwareDefectComponent implements OnInit {
   saveStatus(): void {
     this.hardwareList.filter(x => x.selected === true).forEach(element => {
       this.formDataService.setDefect(element.hardwareID, element.id)
-    })
+    });
+    this.router.navigate(['/HomePage']);
   }
 
   private selectHardware(id: number) {
@@ -29,7 +31,7 @@ export class HardwareDefectComponent implements OnInit {
     $('tr[hardwareid=' + id + '] button').toggleClass('btn-secondairy btn-success').text(element.selected ? 'Geselecteerd!' : 'Selecteer!');
   }
 
-  constructor(private formDataService: FormDataService) { }
+  constructor(private formDataService: FormDataService, private router: Router) { }
   ngOnInit() {
   }
 
